f051db65ac302f8b4baba8213bd0c597d7a6df39
hguochen <hguochen@gmail.com>
2013-10-24 19:58:42 +0800

    [New] Added packing

--- python/packing.py
+++ python/packing.py
@@ -10,10 +10,32 @@ def compute_max_slabs(tray_length, tray_breadth, slab_length, slab_breadth):
 
 	length_slabs = int(tray_length / slab_length)
 	breadth_slabs = int(tray_breadth / slab_breadth)
-	return length_slabs * breadth_slabs
+	
+	remain_length = tray_length - (length_slabs * slab_length)
+	remain_breadth_slab = 0
+
+	remain_breadth = tray_breadth - (breadth_slabs * slab_breadth)
+	remain_length_slab = 0
+
+	if remain_length >= slab_breadth:
+		if tray_breadth >= slab_length:
+			remain_breadth_slab = int(remain_length / slab_breadth)
+	if remain_breadth >= slab_length:
+		if tray_length >= slab_length:
+			remain_length_slab = int(remain_breadth / slab_length)
+	return length_slabs * breadth_slabs +remain_breadth_slab + remain_length_slab
 
 t_length, t_breadth = raw_input('Enter dimensions of tray: ').split()
 s_length, s_breadth = raw_input('Enter dimensions of tray: ').split()
+
+if t_breadth > t_length:
+	temp = t_length
+	t_length = t_breadth
+	t_breadth = temp
+if s_breadth > s_length:
+	temp = s_length
+	s_length = s_breadth
+	s_breadth = temp
 
 tray_length = int(t_length)
 tray_breadth = int(t_breadth)
@@ -22,7 +44,8 @@ slab_breadth = int(s_breadth)
 
 length_base_slab_number = compute_max_slabs(tray_length, tray_breadth, slab_length, slab_breadth)
 breadth_base_slab_number = compute_max_slabs(tray_length, tray_breadth, slab_breadth, slab_length)
-
+print length_base_slab_number
+print breadth_base_slab_number
 if length_base_slab_number > breadth_base_slab_number:
 	print 'Maximum number of slabs =', length_base_slab_number
 else:
