08d59c357884de02c30522a2bfecb722ab0880bd
GuoChen <hguochen@gmail.com>
2014-05-28 22:56:57 +0800

    [Edit] Modified BinaryTree

--- java/BinaryTree.java
+++ java/BinaryTree.java
@@ -37,12 +37,57 @@ public class BinaryTree {
 		}
 	}
 
+	public void preorder(BinaryNode node) {
+		if (node != null) {
+			System.out.print(node.data + " ");
+			preorder(node.leftChild); // traverse left subtree
+			preorder(node.rightChild); // traverse right subtree
+		}
+	}
+
+	public void inorder(BinaryNode node) {
+		if (node == null) {
+			return;
+		}
+		inorder(node.leftChild);
+		System.out.print(node.data + " ");
+		inorder(node.rightChild);
+	}
+
+	public void postorder(BinaryNode node) {
+		if (node == null) {
+			return;
+		}
+		postorder(node.leftChild);
+		postorder(node.rightChild);
+		System.out.print(node.data + " ");
+	}
+
+	public int countNodes(BinaryNode node) {
+		// Returns the number of nodes in the binary tree with root.
+		if (node == null) {
+			return 0;
+		}
+		int count = 1;
+		count = count + countNodes(node.leftChild); // add in nodes in left subtree
+		count = count + countNodes(node.rightChild); // add in nodes in right subtree
+		return count;
+	}
+
 	public static void main(String[] args) {
 		BinaryTree tree = new BinaryTree(5);
 		tree.addNode(tree.root, 3);
-		tree.addNode(tree.root, 7);
-		tree.addNode(tree.root, 2);
+		tree.addNode(tree.root, 4);
 		tree.addNode(tree.root, 9);
+		tree.addNode(tree.root, 7);
 		tree.addNode(tree.root, 6);
+		tree.addNode(tree.root, 8);
+		tree.preorder(tree.root);
+		System.out.println();
+		System.out.println("Total number of nodes: " + tree.countNodes(tree.root));
+		tree.inorder(tree.root);
+		System.out.println();
+		tree.postorder(tree.root);
+		System.out.println();
 	}
 }
--- python/BinaryTree.py
+++ python/BinaryTree.py
@@ -44,9 +44,54 @@ class BinaryTree:
 				node.right = new_node
 				print "Node %d has been added as a right child of %d." % (data, node.data)
 
+	def preorder(self, node):
+		"""
+		Performs a preorder traversal of the binary tree starting from node.
+		"""
+		if node != None:
+			print "%d " % node.data,
+			self.preorder(node.left)
+			self.preorder(node.right)
+
+	def inorder(self, node):
+		"""
+		Performs a inorder traversal of the binary tree starting from node
+		"""
+		if node == None:
+			return
+		self.inorder(node.left)
+		print "%d " % node.data,
+		self.inorder(node.right)
+
+	def postorder(self, node):
+		"""
+		Performs a postorder traversal of the binary tree starting from node
+		"""
+		if node == None:
+			return
+		self.postorder(node.left)
+		self.postorder(node.right)
+		print "%d " % node.data,
+
+	def count_nodes(self, node):
+		"""
+		Returns the number of nodes in the binary tree starting with node
+		"""
+		if (node == None):
+			return 0;
+		return 1 + self.count_nodes(node.left) + self.count_nodes(node.right)
+
 if __name__ == "__main__":
 	tree = BinaryTree(5)
 	tree.add(3, tree.get_root())
+	tree.add(4, tree.get_root())
+	tree.add(9, tree.get_root())
 	tree.add(7, tree.get_root())
-	tree.add(2, tree.get_root())
+	tree.add(6, tree.get_root())
 	tree.add(8, tree.get_root())
+	tree.preorder(tree.get_root())
+	print ""
+	print "Total number of nodes: %d" % tree.count_nodes(tree.get_root())
+	tree.inorder(tree.get_root())
+	print ""
+	tree.postorder(tree.get_root())
