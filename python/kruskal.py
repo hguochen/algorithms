72a2a1ce8183f6c3ee739650389f6fbbf56a9b28
hguochen <hguochen@gmail.com>
2014-06-22 21:38:34 +0800

    [New] Added simple text search and utility functions algorithms

--- python/kruskal.py
+++ python/kruskal.py
@@ -91,6 +91,21 @@ class Weighted_graph:
 		print self.weight
 		print self.vertices
 
+def print_attributes(obj):
+	"""
+	Trverses the items in the object's dictionary and prints each attribute name to its corresponding value
+	"""	
+	for attr in obj.__dict__:
+		print attr, getattr(obj, attr)
+
+def find_defining_class(obj, method_name):
+	"""
+	Uses the method resolution method(MRO) to get the list of class objects(types) that will be searched for methods.
+	Return the class to which the method 'method_name' belong to.
+	"""
+	for origin in type(obj).mro():
+		if method_name in origin.__dict__:
+			return origin
 
 if __name__ == "__main__":
 	test_graph = Weighted_graph([1,2], 4)
