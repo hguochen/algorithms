573a324211380b002d1be84fbe6adc1c5db663ac
hguochen <hguochen@gmail.com>
2014-06-25 18:59:16 +0800

    [New] Added queue linked list implementation

--- python/ADT/Queue.py
+++ python/ADT/Queue.py
@@ -66,14 +66,88 @@ class Node:
 	"""
 	Node structure for Queue_linkedlist class.
 	"""
-	def __init__(self, data=None):
+	def __init__(self, data=None, prev=None, next=None):
 		self.data = data
-		self.left = None
-		self.right = None
+		self.prev = prev
+		self.next = next
 
 class Queue_linkedlist:
 	"""
 	Implementing a queue ADT using a doubly linked list data structure
 	"""
 	def __init__(self):
+		"""
+		Initialize a list when class instance is declared.
+		"""
+		self.queue = None
+		self.tail = None
+		self.size = 0
 
+	def empty(self):
+		"""
+		Return true if queue is empty, else return false
+		"""
+		return self.size == 0
+
+	def front(self):
+		"""
+		Return the item least recently added to the queue, but do not remove it.
+		"""
+		if self.queue == None:
+			return
+		return self.queue.data
+
+	def enqueue(self, value):
+		"""
+		Add the item 'value' to the end of the queue and return the queue.
+		"""
+		new_node = Node(value)
+		if self.queue == None:
+			self.queue = new_node		
+		else:
+			trav = self.queue
+			# set trav to reference the current last node in queue
+			while trav.next != None:
+				trav = trav.next
+			trav.next = new_node
+			new_node.prev = trav
+		self.tail = new_node
+		self.size += 1
+		return self.queue
+
+	def dequeue(self):
+		"""
+		Remove and return the least recently added item from the queue.
+		"""
+		if self.queue != None:
+			data = self.queue.data
+			self.queue = self.queue.next
+			return data
+		else:
+			return
+
+
+	def print_queue(self):
+		"""
+		Print the queue in order from front to back.
+		"""
+		trav = self.queue
+		while trav != None:
+			print "%d " % trav.data, 
+			trav = trav.next
+		print "\n"
+
+"""
+if __name__ == "__main__":
+	test = Queue_linkedlist()
+	test.enqueue(1)
+	test.enqueue(2)
+	test.enqueue(3)
+	test.enqueue(4)
+	test.enqueue(5)
+	test.print_queue()
+	test.dequeue()
+	test.print_queue()
+	test.dequeue()
+	test.print_queue()
+"""
