015459685641e5ca01f3b2327ddcd8fb12b04e42
hguochen <hguochen@gmail.com>
2015-02-20 13:38:23 -0800

    add full solution to minted interview question 2

--- python/minted_interview_2.py
+++ python/minted_interview_2.py
@@ -12,13 +12,17 @@ Given K sorted lists, find the smallest range that includes at least 1 number
 from each list
 
 """
-
-# find the smallest range for 2 sorted lists.
-# List 1: [4, 10, 15, 24, 26]
-# List 2: [0, 9, 12, 20]
+import copy
 
 
-def smallest_range(list1, list2):
+def smallest_range_trivial(list1, list2):
+    """
+    find the smallest range for 2 sorted lists.
+    List 1: [4, 10, 15, 24, 26]
+    List 2: [0, 9, 12, 20]
+    O(2n * m + m) <-> O(n^2)
+
+    """
     # check that both lists are not none
     if len(list1) < 1 or len(list2) < 1:
         return
@@ -28,14 +32,14 @@ def smallest_range(list1, list2):
     contributor = []
     # iterate through the 2 lists and find the list of lesser values and their
     # corresponding contributors
-    for i in range(len(list2)):
+    for i in range(len(list2)):  # O(m)
         # initialize current list to store ranges between each list2 item and
         # each list1 item
         current = []
         # initalize values list to store their corresponding values that
         # contributes to this range
         values = []
-        for j in range(len(list1)):
+        for j in range(len(list1)):  # O(n)
             # put the result of the comparison in each of the 2 lists
             current.append(abs(list2[i]-list1[j]))
             values.append([list2[i], list1[j]])
@@ -44,7 +48,7 @@ def smallest_range(list1, list2):
         lesser.append(min(current))
         # find the contributing values that makes up this lesser value and
         # append to contributor list
-        for i in range(len(current)):
+        for i in range(len(current)):  # O(n)
             if current[i] == min(current):
                 contributor.append(values[i])
                 break
@@ -56,19 +60,81 @@ def smallest_range(list1, list2):
         if lesser[i] < least:
             least = lesser[i]
             result = contributor[i]
-    print least
+    return result
+
+######################################################################
+
+# O(n*k) where n is number for which a list has the most number of items and k
+# is the number of lists
+
+
+def smallest_range(*args):
+    """
+    find smallest_range for a list of lists
+
+    """
+    # unpack arguments
+    lists = list(args)
+
+    # the current set of items in question
+    current = []  # [4, 0, 5] for first set of inputs
+    # initialize index content of every list to 0
+    indexes = [0 for i in lists]
+    # initialize ranges to track the results from each set of currents
+    # ranges has the format [[range_num, [item1, item2, item3]], ...]
+    ranges = []
+
+    # initialize current
+    for i in range(len(lists)):  # O(n)
+        current.append(lists[i][indexes[i]])
+        indexes[i] += 1
+
+    # add the current set into ranges
+    # we use deepcopy here to deassociate the copied current with the changing
+    # current below
+    ranges.append([abs(max(current)-min(current)), copy.deepcopy(current)])
+
+    # set a index for reuse in loop
+    current_index = 1
+    while True:
+        # set a current_index somewhere below
+        try:
+            list_item = lists[current_index][indexes[current_index]]
+        except IndexError:
+            # 1 of the list in lists has been exhausted, end loop here
+            break
+
+        # determine which list item to extract
+        for i in range(len(current)):  # O(n)
+            if current[i] == min(current):
+                # replace with next element in the i-th list
+                current[i] = lists[i][indexes[i]]  # new current generated
+                indexes[i] += 1
+                break
+        # determine the next element to extract and set the current_index
+        for i in range(len(current)):  # O(n)
+            if current[i] == min(current):
+                current_index = i
+        ranges.append([abs(max(current)-min(current)), copy.deepcopy(current)])
+
+    # find the least range and its corresponding result set
+    # set lesser to store the least range as we iterate through the range list
+    # initialize to first range in ranges
+    lesser = ranges[0][0]
+    result = ranges[1]
+
+    for item in ranges:
+        if item[0] < lesser:
+            lesser = item[0]
+            result = item[1]
     return result
 
 if __name__ == "__main__":
     list1 = [4, 10, 15, 24, 26]
     list2 = [0, 9, 12, 20]
     list3 = [5, 18, 22, 30]
-    list4 = [1, 2, 3, 80]
-    list5 = [1, 2, 3, 90, 200]
-    list6 = [1, 2, 3, 99, 300]
-    print smallest_range(list1, list2)
-    print smallest_range(list2, list3)
-    print smallest_range(list1, list3)    
-    print smallest_range(list4, list5)    
-    print smallest_range(list4, list6)    
-    print smallest_range(list5, list6)    
+    print smallest_range_trivial(list1, list2)
+    print smallest_range_trivial(list2, list3)
+    print smallest_range_trivial(list1, list3)
+    print "===="
+    print smallest_range(list1, list2, list3)
