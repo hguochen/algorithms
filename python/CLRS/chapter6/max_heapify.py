11538fa8280bc852fdff9ed00a3465875707139f
hguochen <hguochen@gmail.com>
2015-04-14 18:44:14 -0700

    add priority queue with max heap structure

--- python/CLRS/chapter6/max_heapify.py
+++ python/CLRS/chapter6/max_heapify.py
@@ -10,14 +10,16 @@
 
 def max_heapify(a_list, i):
     # left child index
-    left = 2 * i
+    left = 2 * i + 1
     # right child index
-    right = 2*i + 1
-    if left < len(a_list) and a_list[left] > a_list[i]:
+    right = 2*i + 2
+    largest_index = i
+    if left < len(a_list) and a_list[left] > a_list[largest_index]:
         largest_index = left
-    if right < len(a_list) and a_list[right] > a_list[i]:
-        # right child largest
-        largest_index = right
+    if right < len(a_list) and a_list[right] > a_list[largest_index]:
+        if a_list[right] > a_list[left]:
+            # right child largest
+            largest_index = right
     if largest_index != i:
         a_list[i], a_list[largest_index] = a_list[largest_index], a_list[i]
         return max_heapify(a_list, largest_index)
@@ -30,8 +32,8 @@ def max_heapify_iterative(a_list, i):
 
     """
     # terminating condition: index is more less than heap size
-    left = 2 * i
-    right = 2*i + 1
+    left = 2 * i + 1
+    right = 2*i + 2
 
     while i < len(a_list):
         if a_list[right] > a_list[i]:
@@ -43,3 +45,15 @@ def max_heapify_iterative(a_list, i):
             a_list[i], a_list[largest_index] = a_list[largest_index], a_list[i]
             i = largest_index
     return a_list
+
+
+def build_max_heap(a_list):
+    # build a max heap from given list.
+    for i in range((len(a_list)/2), -1, -1):
+        max_heapify(a_list, i)
+    return a_list
+
+
+if __name__ == "__main__":
+    test_list = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+    print build_max_heap(test_list)
