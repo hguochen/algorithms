e00047fbc628a3f61661e92b135f42f922822327
hguochen <hguochen@gmail.com>
2015-04-30 15:13:51 -0700

    added doubly linked list

---
+++ python/data_structures/lists/doubly_linked_list.py
@@ -0,0 +1,75 @@
+class Node(object):
+    """
+    Node structure for doubly linked list object.
+
+    """
+    def __init__(self, data=None, prev=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+
+class DoubleLinkedList(object):
+    """
+
+    Double linked list with Node object strucure.
+
+    """
+    def __init__(self, data=None, prev=None, next=None):
+        self.head = Node(data, prev, next)
+        self.last = self.head
+
+    def insert(self, data):
+        node = Node(data)
+        self.last.next = node
+        node.prev = self.last
+        self.last = node
+        return
+
+    def delete(self, data):
+        curr = self.head
+        while curr is not None:
+            if curr.data == data:
+                if curr != self.last:
+                    curr.prev.next = curr.next
+                    curr.next.prev = curr.prev
+                else:
+                    curr.prev.next = None
+                    self.last = curr.prev
+                curr = None
+                break
+            curr = curr.next
+        return self.head
+
+    def search(self, data):
+        curr = self.head
+        while curr is not None:
+            if curr.data == data:
+                return curr
+            curr = curr.next
+        return None
+
+    def print_list(self):
+        curr = self.head
+        while curr is not None:
+            print curr.data,
+            curr = curr.next
+        return
+
+if __name__ == "__main__":
+    test = DoubleLinkedList(1)
+    test.insert(2)
+    test.insert(3)
+    test.insert(4)
+    test.insert(5)
+    test.print_list()
+    print "\n"
+    test.delete(2)
+    test.print_list()
+    print "\n"
+    test.delete(3)
+    test.print_list()
+    print "\n"
+    test.delete(4)
+    test.print_list()
+    print "\n"
