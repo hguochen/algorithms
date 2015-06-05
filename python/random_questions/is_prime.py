f4b6b5b1407c265fb61110bbb0ee6f72bc073a05
hguochen <hguochen@gmail.com>
2015-06-05 15:16:40 -0700

    added red black tree insertion

--- python/random_questions/is_prime.py
+++ python/random_questions/is_prime.py
@@ -72,4 +72,32 @@ def primes(number):
 if __name__ == "__main__":
     convertFracts([[1, 2], [1, 3], [1, 4]])
     print primes(121)
-    print prime_factorization(100)
+    print prime_factorization(8736)
+
+
+def prime_factorization(number):
+    """Given a number, give the list of prime factors of the number
+    and return the list of factors"""
+    result = []
+    i = 2
+    while not is_prime(number):
+        if number % i == 0:
+            result.append(i)
+            number /= i
+        else:
+            i += 1
+    result.append(number)
+    return result
+
+
+def is_prime(number):
+    if number == 2 or number == 3:
+        return True
+    elif number % 2 == 0 or number % 3 == 0:
+        return False
+    i = 4
+    while i <= int(math.sqrt(number)):
+        if number % i == 0:
+            return False
+        i += 1
+    return True
