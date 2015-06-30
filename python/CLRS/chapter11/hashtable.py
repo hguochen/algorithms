3f11aa5638acf527881bb02cfd9618d1b555018a
hguochen <hguochen@gmail.com>
2015-06-30 01:14:39 -0700

    added hashtable implementation codes

--- python/CLRS/chapter11/hashtable.py
+++ python/CLRS/chapter11/hashtable.py
@@ -1,3 +1,53 @@
+class HashTable(object):
+    def __init__(self, size):
+        self.size = size
+        self.slots = [None for _ in xrange(self.size)]
+        self.data = [None for _ in xrange(self.size)]
+
+    def insert(self, key, data):
+        hash_value = self.hash(key, len(self.slots))
+
+        if self.slots[hash_value] is None:
+            self.slots[hash_value] = key
+            self.data[hash_value] = data
+        else:
+            if self.slots[hash_value] == key:
+                # replacing with new data
+                self.data[hash_value] = data
+            else:
+                while True:
+                    next = self.rehash(next, len(self.slots))
+                    if self.slots[next] is None or self.slots[next] == key:
+                        break
+                if self.slots[next] is None:
+                    self.slots[next] = key
+                    self.data[next] = data
+                else:
+                    # replace with new data
+                    self.data[next] = data
+
+    def search(self, key):
+        start = self.hash(key, len(self.slots))
+        data = None
+        pos = start
+        while self.slots[pos] is not None:
+            if self.slots[pos] == key:
+                data = self.data[pos]
+                break
+            else:
+                pos = self.rehash(pos, len(self.slots))
+                if pos == start:
+                    break
+        return data
+
+    def hash(self, key, size):
+        return key % size
+
+    def rehash(self, old_hash, size):
+        """Linear probing"""
+        return (old_hash+1) % size
+
+
 class Table(object):
     """
 
@@ -19,7 +69,16 @@ class Table(object):
     def delete(self, value):
         return self.table.pop(self.hashcode(value))
 
-
+    """
+    def hash(string):
+        Hashing using ordinal values of characters
+        total = 0
+        i = 1
+        for pos in string:
+            total += ord(pos) * i
+            i += 1
+        return total % len(self.table)
+    """
 if __name__ == "__main__":
     table = Table()
     table.insert(15)
---
+++ python/data_structures/trees/bst_8.py
@@ -0,0 +1,20 @@
+class Node(object):
+    """
+
+    Node structure for Binary search tree nodes.
+
+    """
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+class BinarySearchTree(object):
+    """
+
+    Implementation of binary search tree with node structure.
+
+    """
+    def __init__(self, data=None):
+        self.root = Node(data)
