41e19194d231a6edbcf3d6c6cc6fc17427f767ef
hguochen <hguochen@gmail.com>
2015-06-30 18:48:04 -0700

    add tree traversals

--- python/data_structures/trees/bst_1.py
+++ python/data_structures/trees/bst_1.py
@@ -18,3 +18,40 @@ class BinarySearchTree(object):
     """
     def __init__(self, data=None):
         self.root = Node(data)
+
+    def preorder(self, node, callback):
+        if node is None:
+            return
+        callback(node)
+        self.preorder(node.left, callback)
+        self.preorder(node.right, callback)
+        return
+
+    def inorder(self, node, callback):
+        if node is None:
+            return
+        self.inorder(node.left, callback)
+        callback(node)
+        self.inorder(node.right, callback)
+        return
+
+    def postorder(self, node, callback):
+        if node is None:
+            return
+        self.postorder(node.left, callback)
+        self.postorder(node.right, callback)
+        callback(node)
+        return
+
+    def levelorder(self, node, callback):
+        if node is None:
+            return
+        queue = [node]
+        while len(queue) > 0:
+            trav = queue.pop(0)
+            callback(queue.pop(0))
+            if trav.left is not None:
+                queue.append(trav.left)
+            if trav.right is not None:
+                queue.append(trav.right)
+        return
--- python/data_structures/trees/bst_10.py
+++ python/data_structures/trees/bst_10.py
@@ -1,7 +1,7 @@
 class Node(object):
     """
 
-    Node structure for Binary search tree nodes.
+    Node sturcture for binary search tree.
 
     """
     def __init__(self, data=None, left=None, right=None):
@@ -13,8 +13,45 @@ class Node(object):
 class BinarySearchTree(object):
     """
 
-    Implementation of binary search tree with node structure.
+    Implementation of Binary search tree with node structure.
 
     """
     def __init__(self, data=None):
         self.root = Node(data)
+
+    def preorder(self, node, callback):
+        if node is None:
+            return
+        callback(node)
+        self.preorder(node.left, callback)
+        self.preorder(node.right, callback)
+        return
+
+    def inorder(self, node, callback):
+        if node is None:
+            return
+        self.inorder(node.left, callback)
+        callback(node)
+        self.inorder(node.right, callback)
+        return
+
+    def postorder(self, node, callback):
+        if node is None:
+            return
+        self.postorder(node.left, callback)
+        self.postorder(node.right, callback)
+        callback(node)
+        return
+
+    def levelorder(self, node, callback):
+        if node is None:
+            return
+        queue = [node]
+        while len(queue) > 0:
+            trav = queue.pop(0)
+            callback(trav)
+            if trav.left is not None:
+                queue.append(trav.left)
+            if trav.right is not None:
+                queue.append(trav.right)
+        return
