0d60697285a50f9f6b52ae24b040faf0320b862a
hguochen <hguochen@gmail.com>
2017-01-21 11:19:07 -0800

    reverse linked list pointer reallocation method

--- php/data_structures/linked_lists/reverse_linked_list.php
+++ php/data_structures/linked_lists/reverse_linked_list.php
@@ -9,6 +9,11 @@ $a->insert(3);
 $a->insert(4);
 $a->printList();
 
+/**
+ * Reverse a linked list via creating a new head.
+ * @param  [type] $head [description]
+ * @return [type]       [description]
+ */
 function reverse($head) {
     if (is_null($head)) {
         return $head;
@@ -28,4 +33,30 @@ function reverse($head) {
     }
 }
 
+/**
+ * Reverse a linked list via pointer reallocation
+ */
+function reverseV2($head) {
+    if (is_null($head)) {
+        return $head;
+    }
+    $prev = NULL;
+    $curr = $head;
+
+    while (!is_null($curr)) {
+        $next = $curr->getNext();
+        $curr->setNext($prev);
+        $prev = $curr;
+        $curr = $next;
+    }
+    $head = $prev;
+
+    $newCurr = $head;
+    while (!is_null($newCurr)) {
+        echo $newCurr->getData() . PHP_EOL;
+        $newCurr = $newCurr->getNext();
+    }
+}
+
 reverse($a->getHead());
+reverseV2($a->getHead());
