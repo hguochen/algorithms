957ea9d05b52e92f0ce2e694a4f9c06ada967ff0
hguochen <hguochen@gmail.com>
2017-01-24 02:31:21 -0800

    implemented invert binary tree method

--- php/data_structures/binary_trees/binary_tree.php
+++ php/data_structures/binary_trees/binary_tree.php
@@ -61,29 +61,29 @@ class BinaryTree {
     }
 }
 
-$tree = new BinaryTree(1);
-$root = $tree->getRoot();
+// $tree = new BinaryTree(1);
+// $root = $tree->getRoot();
 
-$root->left = new Node(2);
-$node2 = $root->left;
-$root->right = new Node(3);
-$node3 = $root->right;
+// $root->left = new Node(2);
+// $node2 = $root->left;
+// $root->right = new Node(3);
+// $node3 = $root->right;
 
-$node2->right = new Node(4);
+// $node2->right = new Node(4);
 
-$node3->left = new Node(5);
-$node5 = $node3->left;
-$node5->left = new Node(6);
-$node5->right = new Node(7);
+// $node3->left = new Node(5);
+// $node5 = $node3->left;
+// $node5->left = new Node(6);
+// $node5->right = new Node(7);
 
-echo $tree->nodeCount($root);
-echo PHP_EOL;
-echo "preorder traversal: " ;
-$tree->preorder($root);
-echo PHP_EOL;
-echo "inorder traversal: ";
-$tree->inorder($root);
-echo PHP_EOL;
-echo "postorder traversal: ";
-$tree->postorder($root);
-echo PHP_EOL;
+// echo $tree->nodeCount($root);
+// echo PHP_EOL;
+// echo "preorder traversal: " ;
+// $tree->preorder($root);
+// echo PHP_EOL;
+// echo "inorder traversal: ";
+// $tree->inorder($root);
+// echo PHP_EOL;
+// echo "postorder traversal: ";
+// $tree->postorder($root);
+// echo PHP_EOL;
