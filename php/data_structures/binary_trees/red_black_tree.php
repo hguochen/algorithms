a199a06b560445f89407316c1265159c3264990d
hguochen <hguochen@gmail.com>
2017-02-01 15:10:47 -0800

    adding red black tree insert fix up implementation

--- php/data_structures/binary_trees/red_black_tree.php
+++ php/data_structures/binary_trees/red_black_tree.php
@@ -21,7 +21,94 @@ class RedBlackTree {
         $this->root = new Node($data, $isRed, $left, $right);
     }
 
-    private function leftRotate($node) {
+    public function insert($data) {
+        // insert new data as a red node
+        $newNode = new Node($data, true);
+        if (empty($this->root)) {
+            $this->root = $newNode;
+        }
+        $this->insertNode($this->root, $newNode);
+        $this->insertFixUp($newNode);
+        return $this->root;
+    }
+
+    private function insertNode(&$targetNode, &$newNode) {
+        if ($newNode->data < $targetNode->data) {
+            if (empty($targetNode->left)) {
+                $targetNode->left = $newNode;
+            } else {
+                $this->insertNode($targetNode->left, $newNode);
+            }
+        } else {
+            if (empty($targetNode->right)) {
+                $targetNode->right = $newNode;
+            } else {
+                $this->insertNode($targetNode->right, $newNode);
+            }
+        }
+    }
+
+    /**
+     * Restore red black tree properties after an insert operation.
+     */
+    private function insertFixUp(&$node) {
+        $parent = $this->getParentNode($node);
+        $grandParent = $this->getParentNode($parent);
+        while ($parent->isRed) {
+            if ($parent == $grandParent->left) {
+                // node's parent is a left child
+                $uncle = $grandParent->right;
+
+                if ($uncle->isRed) {
+                    // case 1: inserted node and its parent are red, uncle is red
+                    // set parent and uncle to black
+                    // set grandparent to red
+                    // move node reference to grandparent
+                    $parent->isRed = false;
+                    $uncle->isRed = false;
+                    $grandParent->isRed = true;
+                    $node = $grandParent;
+                } elseif ($node == $parent->right) { // node is a right child
+                    // case 2: node is a right child, uncle is black
+                    // left rotate on node's parent
+                    $node = $parent;
+                    $this->leftRotate($node);
+                }
+                // case 3: node is left child, uncle is black
+                // set parent to black
+                // set grandparent to red and rotate right on grandparent
+                $parent->isRed = false;
+                $grandParent->isRed = true;
+                $this->rightRotate($grandparent);
+            } else { // $parent is a right child of $grandParent
+                $uncle = $grandParent->left;
+
+                if ($uncle->isRed) {
+                    // case 4: inserted node and its parent are red, uncle is red
+                    // set parent and uncle to black
+                    // set grandparent to red
+                    // move node reference to grandparent
+                    $parent->isRed = false;
+                    $uncle->isRed = false;
+                    $grandParent->isRed = true;
+                    $node = $grandParent;
+                } elseif ($node == $parent->left) { // node is left child
+                    // case 5: node is a left child, uncle is black
+                    // right rotate on parent
+                    $node = $parent;
+                    $this->rightRotate($node);
+                }
+                // case 6: node is a right child, uncle is black
+                // set parent to black
+                // set grandparent to red and rotate left on grandparent
+                $parent->isRed = false;
+                $grandParent->isRed = true;
+                $this->leftRotate($grandParent);
+            }
+        }
+    }
+
+    private function leftRotate(&$node) {
         // set y reference to node's right child
         $rightNode = $node->right;
         // set node's right child to y's left child
@@ -39,7 +126,7 @@ class RedBlackTree {
         $rightNode->left = $node;
     }
 
-    private function rightRotate($node) {
+    private function rightRotate(&$node) {
         // set y as node.left reference
         // set node.left to y.right
         // find parent node
