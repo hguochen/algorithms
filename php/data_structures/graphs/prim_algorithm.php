cdfe1a77c768d93a42c44e4fb89344126f91b531
hguochen <hguochen@gmail.com>
2017-02-05 12:39:32 -0800

    implement prim's algorithm

--- php/data_structures/graphs/prim_algorithm.php
+++ php/data_structures/graphs/prim_algorithm.php
@@ -2,6 +2,52 @@
 
 include "adjacency_list_2.php";
 
+function primMST(Graph $graph, $startIndex) {
+    // init mst bool array
+    // init keys array to store weight values of all vertices
+    // init parents array to store indexes of parent nodes mst
+    $mst = array_fill(0, $graph->getVerticeCount(), False);
+    $keys = array_fill(0, $graph->getVerticeCount(), PHP_INT_MAX);
+    $parents = array_fill(0, $graph->getVerticeCount(), NULL);
+    
+    // set startindex key to 0
+    $keys[$startIndex] = 0;
+    $index = $startIndex;
+    // while there are vertices not visited
+    while (in_array(False, $mst)) {
+        // set this vertice as visited
+        $mst[$index] = True;
+        // update adjacent edge vertices of this vertex
+        $curr = $graph->getEdges()[$index];
+        while (!empty($curr)) {
+            // if current vertex is not visited, and found weight is lesser than key weight
+            //     set found weight to key weight
+            //     set current vertex parent to index vertex above
+            if (!$mst[$curr->data] && $keys[$curr->data] > $curr->weight) {
+                $keys[$curr->data] = $curr->weight;
+                $parents[$curr->data] = $index;
+            }
+            // move to next edge
+            $curr = $curr->next;
+        }
+        // find the minimum weight and set it's index
+        $index = getMinWeightIndex($graph, $mst, $keys);
+    }
+    return $parents;
+}
+
+function getMinWeightIndex(Graph $graph, $mst, $keys) {
+    $minDist = PHP_INT_MAX;
+    $index = -1;
+    for ($i=0; $i < $graph->getVerticeCount(); $i++) { 
+        if(!$mst[$i] && $minDist > $keys[$i]) {
+            $minDist = $keys[$i];
+            $index = $i;
+        }
+    }
+    return $index;
+}
+
 $graph = new Graph(9, 0, False);
 $graph->insertEdge(0, 1, 4);
 $graph->insertEdge(0, 7, 8);
@@ -39,3 +85,5 @@ $graph->insertEdge(8, 2, 2);
 $graph->insertEdge(8, 6, 6);
 $graph->insertEdge(8, 7, 7);
 $graph->printGraph();
+
+echo implode(" ", primMST($graph, 0)) . PHP_EOL;
