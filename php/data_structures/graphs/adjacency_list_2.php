454e0a00f83d2aab190b592e66da1260ce73e1aa
hguochen <hguochen@gmail.com>
2017-02-04 23:27:23 -0800

    set the board for prim's algorithm

--- php/data_structures/graphs/adjacency_list_2.php
+++ php/data_structures/graphs/adjacency_list_2.php
@@ -80,40 +80,40 @@ class Graph {
     }
 }
 
-$graph = new Graph(9, 0, False);
-$graph->insertEdge(0, 1, 4);
-$graph->insertEdge(0, 7, 8);
+// $graph = new Graph(9, 0, False);
+// $graph->insertEdge(0, 1, 4);
+// $graph->insertEdge(0, 7, 8);
 
-$graph->insertEdge(1, 2, 8);
-$graph->insertEdge(1, 7, 11);
+// $graph->insertEdge(1, 2, 8);
+// $graph->insertEdge(1, 7, 11);
 
-$graph->insertEdge(2, 1, 8);
-$graph->insertEdge(2, 3, 7);
-$graph->insertEdge(2, 5, 4);
-$graph->insertEdge(2, 8, 2);
+// $graph->insertEdge(2, 1, 8);
+// $graph->insertEdge(2, 3, 7);
+// $graph->insertEdge(2, 5, 4);
+// $graph->insertEdge(2, 8, 2);
 
-$graph->insertEdge(3, 2, 7);
-$graph->insertEdge(3, 4, 9);
-$graph->insertEdge(3, 5, 14);
+// $graph->insertEdge(3, 2, 7);
+// $graph->insertEdge(3, 4, 9);
+// $graph->insertEdge(3, 5, 14);
 
-$graph->insertEdge(4, 3, 9);
-$graph->insertEdge(4, 5, 10);
+// $graph->insertEdge(4, 3, 9);
+// $graph->insertEdge(4, 5, 10);
 
-$graph->insertEdge(5, 2, 4);
-$graph->insertEdge(5, 3, 14);
-$graph->insertEdge(5, 4, 10);
-$graph->insertEdge(5, 6, 2);
+// $graph->insertEdge(5, 2, 4);
+// $graph->insertEdge(5, 3, 14);
+// $graph->insertEdge(5, 4, 10);
+// $graph->insertEdge(5, 6, 2);
 
-$graph->insertEdge(6, 5, 2);
-$graph->insertEdge(6, 7, 1);
-$graph->insertEdge(6, 8, 6);
+// $graph->insertEdge(6, 5, 2);
+// $graph->insertEdge(6, 7, 1);
+// $graph->insertEdge(6, 8, 6);
 
-$graph->insertEdge(7, 0, 8);
-$graph->insertEdge(7, 1, 11);
-$graph->insertEdge(7, 8, 7);
-$graph->insertEdge(7, 6, 1);
+// $graph->insertEdge(7, 0, 8);
+// $graph->insertEdge(7, 1, 11);
+// $graph->insertEdge(7, 8, 7);
+// $graph->insertEdge(7, 6, 1);
 
-$graph->insertEdge(8, 2, 2);
-$graph->insertEdge(8, 6, 6);
-$graph->insertEdge(8, 7, 7);
-$graph->printGraph();
+// $graph->insertEdge(8, 2, 2);
+// $graph->insertEdge(8, 6, 6);
+// $graph->insertEdge(8, 7, 7);
+// $graph->printGraph();
