ef798b97994f9df46ce57e8d8bd7310ab6d0cbff
hguochen <hguochen@gmail.com>
2017-02-10 11:51:56 -0800

    fibonacci DP solution

--- php/recursion/fibonacci.php
+++ php/recursion/fibonacci.php
@@ -5,7 +5,9 @@
  * Basic recursive fibonacci sequence.
  */
 function fibonacci($n) {
-    if ($n <= 2) {
+    if ($n <= 0) {
+        return 0;
+    } elseif ($n <= 2) {
         return 1;
     }
     return fibonacci($n-1) + fibonacci($n-2);
@@ -15,12 +17,79 @@ function fibonacci($n) {
  * Iterative fibonacci number generation.
  */
 function fibonacciIterative($n) {
-    if ($n <= 2) {
+    if ($n <= 0) {
+        return 0;
+    } elseif ($n <= 2) {
         return 1;
     }
     $prev = 1;
     $curr = 1;
     for ($i=3; $i <= $n; $i++) { 
+        $temp = $curr;
+        $curr += $prev;
+        $prev = $temp;
+    }
+    return $curr;
+}
+
+/**
+ * Caching solution to computing fibonacci number.
+ * For each value of N, _fibonacciCache is called exactly twice.
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ *
+ * Here we are trading a linear amount of space for an exponential amount of time.
+ */
+function fibonacciCache($n) {
+    $fiboSequence = [];
+    $fiboSequence[0] = 0;
+    $fiboSequence[1] = 1;
+
+    return _fibonacciCache($n, $fiboSequence);
+
+}
+
+function _fibonacciCache($n, &$sequence) {
+    if (!isset($sequence[$n])) {
+        $sequence[$n] = _fibonacciCache($n-1, $sequence) + _fibonacciCache($n-2, $sequence);
+    }
+    return $sequence[$n];
+}
+
+/**
+ * Dynamic Programming solution to computing fibonacci number.
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+function fibonacciDP($n) {
+    $fiboSequence = [];
+    $fiboSequence[0] = 0;
+    $fiboSequence[1] = 1;
+
+    for ($i=2; $i <= $n; $i++) { 
+        $fiboSequence[$i] = $fiboSequence[$i-1] + $fiboSequence[$i-2];
+    }
+    return $fiboSequence[$n];
+}
+
+/**
+ * Dynamic programming solution to computing fibonacci number.
+ * Most optimized solution for space and time.
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ *
+ * This solution reduces store demands to constant space with no asymptotic degradation in running time.
+ */
+function fibonacciDP2($n) {
+    if ($n == 0) {
+        return 0;
+    } elseif ($n == 1) {
+        return 1;
+    }
+    $prev = 0;
+    $curr = 1;
+
+    for ($i=2; $i <= $n ; $i++) { 
         $temp = $curr;
         $curr += $prev;
         $prev = $temp;
@@ -48,3 +117,22 @@ echo fibonacciIterative(7) . PHP_EOL;
 echo fibonacciIterative(8) . PHP_EOL;
 echo fibonacciIterative(9) . PHP_EOL;
 
+$start = microtime(true);
+echo fibonacciDP2(40) . PHP_EOL;
+$elapsedTime = microtime(true) - $start;
+echo "optimal DP elapsed time: " . $elapsedTime . PHP_EOL;
+
+$start = microtime(true);
+echo fibonacciDP(40) . PHP_EOL;
+$elapsedTime = microtime(true) - $start;
+echo "DP elapsed time: " . $elapsedTime . PHP_EOL;
+
+$start = microtime(true);
+echo fibonacciCache(40) . PHP_EOL;
+$elapsedTime = microtime(true) - $start;
+echo "cache elapsed time: " . $elapsedTime . PHP_EOL;
+
+$start = microtime(true);
+echo fibonacci(40) . PHP_EOL;
+$elapsedTime = microtime(true) - $start;
+echo "no cache elapsed time: " . $elapsedTime . PHP_EOL;
