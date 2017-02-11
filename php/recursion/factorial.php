86d1d12dac3a6239139617892d20a7d6320930fb
hguochen <hguochen@gmail.com>
2017-02-11 13:59:11 -0800

    recursion practice

--- php/recursion/factorial.php
+++ php/recursion/factorial.php
@@ -1,5 +1,12 @@
 <?php
 
+
+// Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+
+// factorial(1) -> 1
+// factorial(2) -> 2
+// factorial(3) -> 6
+
 function factorial($n) {
     if ($n == 0) {
         return 1;
