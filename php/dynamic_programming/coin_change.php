f4353923b82e129f5b442ea68207f7daa23c25c7
hguochen <hguochen@gmail.com>
2017-02-13 10:56:00 -0800

    get coin change optimal denominations

--- php/dynamic_programming/coin_change.php
+++ php/dynamic_programming/coin_change.php
@@ -12,21 +12,41 @@ function coinChange($denom, $value) {
     // init coins table
     $perDenom = array_fill(0, $value+1, -1);
     $coins = array_fill(0, sizeof($denom), $perDenom);
+    // init used array
+    $perRow = array_fill(0, $value+1, False);
+    $used = array_fill(0, sizeof($denom), $perRow);
+
     // loop through every element and compute coin numbers
     for ($i=0; $i < sizeof($denom); $i++) { 
         for ($j=0; $j < $value+1; $j++) { 
             if ($i == 0) {
                 $coins[$i][$j] = $j;
+                $used[$i][$j] = True;
             } else {
-                if ($denom[$i] > $j) {
+                if ($denom[$i] > $j || $coins[$i-1][$j] < 1 + $coins[$i][$j - $denom[$i]]) {
                     $coins[$i][$j] = $coins[$i-1][$j];
+                    $used[$i][$j] = False;
                 } else {
-                    $coins[$i][$j] = min($coins[$i-1][$j], 1 + $coins[$i][$j - $denom[$i]]);
+                    $coins[$i][$j] = 1 + $coins[$i][$j - $denom[$i]];
+                    $used[$i][$j] = True;
                 }
             }
         }
     }
+    optimalCoins(sizeof($denom)-1, $value, $denom, $used);
     return $coins[sizeof($denom)-1][$value];
+}
+
+function optimalCoins($i, $j, $denom, $used) {
+    if ($j == 0) {
+        return;
+    }
+    if ($used[$i][$j]) {
+        echo "Used a coin of denomination: " . $denom[$i] . PHP_EOL;
+        optimalCoins($i, $j-$denom[$i], $denom, $used);
+    } else {
+        optimalCoins($i-1, $j, $denom, $used);
+    }
 }
 
 function print2Darray($arr) {
