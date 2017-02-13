187333b9b700cda48f91060fbb27a78a9f7e4b29
hguochen <hguochen@gmail.com>
2017-02-13 10:07:21 -0800

    coin change problem

---
+++ php/dynamic_programming/coin_change.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+Given a number of coin denominations, what's the fewest number of coins to return
+if we are to return A coins?
+ */
+
+// mission: for A coins, find out the fewest number of coins to return that makes up
+// exactly A coin value.
+
+function coinChange($denom, $value) {
+    // init coins table
+    $perDenom = array_fill(0, $value+1, -1);
+    $coins = array_fill(0, sizeof($denom), $perDenom);
+    // loop through every element and compute coin numbers
+    for ($i=0; $i < sizeof($denom); $i++) { 
+        for ($j=0; $j < $value+1; $j++) { 
+            if ($i == 0) {
+                $coins[$i][$j] = $j;
+            } else {
+                if ($denom[$i] > $j) {
+                    $coins[$i][$j] = $coins[$i-1][$j];
+                } else {
+                    $coins[$i][$j] = min($coins[$i-1][$j], 1 + $coins[$i][$j - $denom[$i]]);
+                }
+            }
+        }
+    }
+    return $coins[sizeof($denom)-1][$value];
+}
+
+function print2Darray($arr) {
+    for ($i=0; $i < sizeof($arr); $i++) { 
+        echo implode(" ", $arr[$i]) . PHP_EOL;
+    }
+}
+
+$denom = [
+    0 => 1,
+    1 => 6,
+    2 => 10
+];
+
+echo coinChange($denom, 12);
