0bd7d0cef7417f022b9a138a7165102d1fa0a29a
hguochen <hguochen@gmail.com>
2017-02-16 01:32:18 -0800

    dp longest palindromic substring

--- php/dynamic_programming/longest_palindrome_substring.php
+++ php/dynamic_programming/longest_palindrome_substring.php
@@ -25,6 +25,14 @@ function longestPalindrome($string) {
     return $result;
 }
 
+/**
+ * Find the longest palindrome substring.
+ * A version of manacher's algorithm.
+ * Put a wild character '*' in between every adjacent character to factor in even and odd palindromes.
+ * 
+ * Time complexity: O(n^2)
+ * Space complexity: O(n)
+ */
 function longestPalindromeV2($string) {
     if (empty($string)) {
         return;
@@ -52,6 +60,54 @@ function longestPalindromeV2($string) {
         }
     }
     return str_replace("*", "", $result);
+}
+
+function longestPalindromeV3($string) {
+    $temp = array_fill(0, strlen($string), -1);
+    $table = array_fill(0, strlen($string), $temp);
+    
+    // all substrings of length 1 are palindromes
+    for ($i=0; $i < strlen($string); $i++) { 
+        $table[$i][$i] = 1;
+    }
+    // check for substring of length 2
+    $start = 0;
+    $maxLength = 1;
+    for ($i=0; $i < strlen($string)-1; $i++) { 
+        if ($string[$i] == $str[$i+1]) {
+            $table[$i][$i+1] = 1;
+            $start = $i;
+            $maxLength = 2;
+        }
+    }
+
+    // check for lengths greater than 2, k is length of substring
+    for ($k=3; $k <= strlen($string); $k++) { 
+        for ($i=0; $i < strlen($string)-$k+1; $i++) { 
+            // Get the ending index of substring from
+            // starting index i and length k
+            $j = $i + $k - 1;
+
+            // check for substring from ith index to jth index
+            // iff str[i+1] to str[j-1] is a palindrome
+            if ($table[$i+1][$j-1] && $string[$i] == $string[$j]) {
+                $table[$i][$j] = 1;
+                if ($k > maxLength) {
+                    $start = $i;
+                    $maxLength = $k;
+                }
+            }
+        }
+    }
+    // print2Darray($table);
+    return substr($string, $start, $maxLength);
+    // return $maxLength;
+}
+
+function print2Darray($arr) {
+    for ($i=0; $i < sizeof($arr); $i++) { 
+        echo implode(" ", $arr[$i]) . PHP_EOL;
+    }
 }
 
 /**
@@ -83,10 +139,14 @@ $string5 = "forgeeksskeegfor";
 // echo isPalindrome($string3, 0, strlen($string3)-1) . PHP_EOL; // false
 // echo isPalindrome($string4, 0, strlen($string4)-1) . PHP_EOL; // true
 
-// echo longestPalindrome($string5) . PHP_EOL;
+// echo longestPalindromeV2($string1) . PHP_EOL;
+// echo longestPalindromeV2($string2) . PHP_EOL;
+// echo longestPalindromeV2($string3) . PHP_EOL;
+// echo longestPalindromeV2($string4) . PHP_EOL;
+// echo longestPalindromeV2($string5) . PHP_EOL;
 
-echo longestPalindromeV2($string1) . PHP_EOL;
-echo longestPalindromeV2($string2) . PHP_EOL;
-echo longestPalindromeV2($string3) . PHP_EOL;
-echo longestPalindromeV2($string4) . PHP_EOL;
-echo longestPalindromeV2($string5) . PHP_EOL;
+echo longestPalindromeV3($string1) . PHP_EOL;
+echo longestPalindromeV3($string2) . PHP_EOL;
+echo longestPalindromeV3($string3) . PHP_EOL;
+echo longestPalindromeV3($string4) . PHP_EOL;
+echo longestPalindromeV3($string5) . PHP_EOL;
