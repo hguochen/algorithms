7459ab1d4df1aa6a2301776325f5505b6e2fa243
hguochen <hguochen@gmail.com>
2017-02-23 13:48:52 -0800

    sorted merge problem

---
+++ php/cracking_coding_interview/chapter9/chapter10/sorted_merge.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * You are given two sorted arrays, A and B, where A has a large enough buffer at
+ * the end to hold B. Write a method to merge B into A in sorted order.
+ */
+
+/**
+ * Time: O(n+m) where n is size of large array, m is size of small array
+ * Space: O(n+m)
+ * 
+ */
+function sortedMerge($large, $small) {
+    // put small array into bigger array
+    $smallIndex = -1;
+    for ($i=0; $i < sizeof($large); $i++) { 
+        if (empty($large[$i])) {
+            if ($smallIndex == -1) {
+                $smallIndex = $i;
+            }
+            $large[$i] = array_shift($small);
+        }
+    }
+    // merge them into 1 array with merge part of merge sort
+    return merge($large, $smallIndex);
+}
+
+function merge($array, $rightIndex) {
+    $temp = [];
+    $leftStart = 0;
+    $leftEnd = $rightIndex - 1;
+    $rightEnd = sizeof($array) - 1;
+    while ($leftStart <= $leftEnd && $rightIndex <= $rightEnd) {
+        if ($array[$leftStart] < $array[$rightIndex]) {
+            $temp[] = $array[$leftStart];
+            $leftStart++;
+        } else {
+            $temp[] = $array[$rightIndex];
+            $rightIndex++;
+        }
+    }
+    while ($leftStart <= $leftEnd) {
+        $temp[] = $array[$leftStart];
+        $leftStart++;
+    }
+    while ($rightIndex <= $rightEnd) {
+        $temp[] = $array[$rightIndex];
+        $rightIndex++;
+    }
+    return $temp;
+}
+
+$input1 = [10, 14, 19, 27, 33, 35, 42, 44, NULL, NULL, NULL, NULL, NULL];
+$input2 = [11, 12, 22, 25, 64];
+
+echo implode(" ", sortedMerge($input1, $input2)) . PHP_EOL;
