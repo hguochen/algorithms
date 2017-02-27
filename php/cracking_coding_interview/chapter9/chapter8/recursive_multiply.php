acb7fdbc834dd7e00c053f171f0009b8c7a0c0fd
hguochen <hguochen@gmail.com>
2017-02-26 22:39:04 -0800

    magic index problem

---
+++ php/cracking_coding_interview/chapter9/chapter8/recursive_multiply.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Write a recursive function to multiply two positive integers without using the
+ * * operator. You can use addition, subtraction, and bit shifting, but you should
+ * minimize the number of those operations.
+ */
