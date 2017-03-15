671fb6b58db8497f6a50dc0e42cd96fbae9a4c53
hguochen <hguochen@gmail.com>
2017-03-14 22:35:58 -0700

    added json file scrubbing problem

--- php/files/fileOpen.php
+++ php/files/fileOpen.php
@@ -1,6 +1,6 @@
 <?php
 
-$file = getcwd() . '/files/test_file.txt' or die('Could not open file');
+$file = getcwd() . '/test_file.txt' or die('Could not open file');
 
 $fh = fopen($file, 'r') or die('Could not open file');
 
@@ -9,12 +9,12 @@ $data = fread($fh, filesize($file)) or die('Could not read file!');
 fclose($fh);
 
 echo $data;
-echo 'File size: ' . filesize($file) . ';';
-echo 'File owner: ' . fileowner($file) . ';';
-echo 'File group: ' . filegroup($file) . ';';
-echo 'File permissions ' . fileperms($file) . ';';
-echo 'File type: ' . filetype($file) . ';';
-echo 'File last accessed on: ' . date('Y-m-d', fileatime($file)) . ';';
-echo 'File last modified on: ' . date('Y-m-d', filemtime($file)) . ';'
+echo 'File size: ' . filesize($file) . ';' . PHP_EOL;
+echo 'File owner: ' . fileowner($file) . ';' . PHP_EOL;
+echo 'File group: ' . filegroup($file) . ';' . PHP_EOL;
+echo 'File permissions ' . fileperms($file) . ';' . PHP_EOL;
+echo 'File type: ' . filetype($file) . ';' . PHP_EOL;
+// echo 'File last accessed on: ' . date('Y-m-d', fileatime($file)) . ';' . PHP_EOL;
+// echo 'File last modified on: ' . date('Y-m-d', filemtime($file)) . ';' . PHP_EOL;
 
 ?>
