87ebcf218cbc34f5d4768ff7aaa579ffca300a07
hguochen <hguochen@gmail.com>
2017-03-23 14:58:22 -0700

    implement powerset recursive without using for loop

--- php/questions/arrays/powersets.php
+++ php/questions/arrays/powersets.php
@@ -48,10 +48,12 @@ function generatePowersetHelper($array, &$result, $index) {
         return;
     }
     $temp = [[]];
+    // add new char to each of the result elements
     foreach ($result as $value) {
         $value[] = $array[$index];
         $temp[] = $value;
     }
+    // put elements into result if its new
     foreach ($temp as $set) {
         if (!in_array($set, $result)) {
             $result[] = $set;
@@ -60,7 +62,42 @@ function generatePowersetHelper($array, &$result, $index) {
     generatePowersetHelper($array, $result, ++$index);
 }
 
+function generatePowersetsRecursionV2($array) {
+    $result = [[]];
+    if (empty($array)) {
+        return $result;
+    }
+    for ($i=0; $i < sizeof($array); $i++) { 
+        generatePowersetHelperV2($array, $result, 0, $i);
+    }
+
+    return $result;
+}
+
+function generatePowersetHelperV2($array, &$result, $resultIndex, $arrayIndex) {
+    // if current result element already has the new char, terminate
+    if (in_array($array[$arrayIndex], $result[$resultIndex])) {
+        if (!in_array([], $result)) {
+            $result[] = [];
+        }
+        return;
+    }
+    // new element is not in current result
+    // append new element to end of current result element
+    $temp = $result[$resultIndex];
+    $temp[] = $array[$arrayIndex];
+    if (!in_array($temp, $result)) {
+        $result[] = $temp;
+    }
+    
+    generatePowersetHelperV2($array, $result, ++$resultIndex, $arrayIndex);
+}
+
+
+
+
 $arr1 = ['A', 'B', 'C', 'D', 'E'];
 
-print_r(generatePowersets($arr1));
-print_r(generatePowersetsRecursion($arr1));
+// print_r(generatePowersets($arr1));
+// print_r(generatePowersetsRecursion($arr1));
+print_r(generatePowersetsRecursionV2($arr1));
